0fe9e097d915abbc17d62f1fe8ecdd3ccec9e53d
Markus Bukowski <me@mrbuk.de>
2018-12-31 15:04:17 +0100

    Check provided mac format and if broadcast exists

--- cmd/wol.go
+++ cmd/wol.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"os"
 
 	wol "github.com/ghthor/gowol"
@@ -15,14 +18,54 @@ func main() {
 
 	flag.Parse()
 
+	// check for required arguments
 	if *macPtr == "" || *broadcastPtr == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	log.Printf("Sending WOL packet to '%s'\n", *macPtr)
-	err := wol.MagicWake(*macPtr, *broadcastPtr)
-	if err != nil {
-		log.Fatalln(err)
+	// check if provided mac is valid
+	if _, err := net.ParseMAC(*macPtr); err != nil {
+		log.Fatalf("Error: %s", err)
 	}
+
+	// check if provided broadcast is reachable
+	err := checkBroadcast(*broadcastPtr)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+	log.Printf("Sending WOL packet to '%s'\n", *macPtr)
+	err = wol.MagicWake(*macPtr, *broadcastPtr)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+}
+
+func checkBroadcast(broadcastString string) error {
+	broadcast := net.ParseIP(broadcastString)
+	if broadcast == nil {
+		return errors.New(fmt.Sprintf("%s not a valid IP address", broadcastString))
+	}
+	log.Printf("Using '%s' as broadcast", broadcast)
+
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		return err
+	}
+	for _, i := range interfaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			log.Printf("Error occured checking interface '%v'. Error: %s\n", i, err.Error())
+		}
+		for _, addr := range addrs {
+			if ipaddr, ok := addr.(*net.IPNet); ok {
+				if ipaddr.Contains(broadcast) {
+					return nil
+				}
+			}
+		}
+	}
+
+	return errors.New(fmt.Sprintf("Could not find interface containing broadcast %s", broadcast))
 }
